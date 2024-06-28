4e45dcbac72abb33824f0a27de1e804b4eda6ac8
gabrprog <g.alberto.dev@gmail.com>
2024-06-28 11:12:44 -0300

    prova terminada

---
+++ avaliação a1/src/br/edu/up/modelos/Aluno.java
@@ -0,0 +1,25 @@
+package br.edu.up.modelos;
+
+public class Aluno {
+    private int matricula;
+    private String nome;
+    private double nota;
+
+    public Aluno(int matricula, String nome, double nota) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+}
